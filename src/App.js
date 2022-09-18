473e66cd899361da4db94a78505db3a4758ab914
redha-boubakour <redha.boubakour@yahoo.fr>
2022-09-18 15:52:33 +0200

    commit : anecdote ex

--- src/App.js
+++ src/App.js
@@ -1,88 +1,47 @@
 import { useState } from "react";
 
-const Button = (props) => {
-    return (
-        <>
-            <button onClick={props.onClick}>{props.text}</button>
-        </>
-    );
-};
-
-const StatisticLine = (props) => (
-    <tr>
-        <td>{props.text}</td>
-        <td>{props.value}</td>
-    </tr>
-);
-
-const Statistics = (props) => {
-    return (
-        <>
-            <table>
-                <tbody>
-                    <StatisticLine text="good" value={props.good} />
-                    <StatisticLine text="neutral" value={props.neutral} />
-                    <StatisticLine text="bad" value={props.bad} />
-
-                    <tr>
-                        <td>all</td>
-                        <td>{props.all}</td>
-                    </tr>
-                    <tr>
-                        <td>average</td>
-                        <td>{props.average}</td>
-                    </tr>
-                    <tr>
-                        <td>positive</td>
-                        <td>{props.positive} %</td>
-                    </tr>
-                </tbody>
-            </table>
-        </>
-    );
-};
-
 const App = () => {
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
+    const anecdotes = [
+        "If it hurts, do it more often.",
+        "Adding manpower to a late software project makes it later!",
+        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+        "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        "Premature optimization is the root of all evil.",
+        "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+        "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    ];
 
-    const all = good + neutral + bad;
-    const average = (good - bad) / all;
-    const positive = (good / all) * 100;
+    const [selected, setSelected] = useState(0);
+    const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
-    const handleClickGood = () => {
-        setGood(good + 1);
+    let indexOfMaxValue = votes.reduce(
+        (iMax, x, i, arr) => (x > arr[iMax] ? i : iMax),
+        0
+    );
+
+    const handleShow = () => {
+        setSelected(Math.floor(Math.random() * anecdotes.length));
     };
 
-    const handleClickNeutral = () => {
-        setNeutral(neutral + 1);
+    const handleVote = () => {
+        const votesCopy = [...votes];
+        votesCopy[selected] += 1;
+        setVotes(votesCopy);
     };
 
-    const handleClickBad = () => {
-        setBad(bad + 1);
-    };
+    console.log(votes);
 
     return (
         <>
-            <h3>Give feedback</h3>
-            <Button onClick={handleClickGood} text={"Good"} />
-            <Button onClick={handleClickNeutral} text={"Neutral"} />
-            <Button onClick={handleClickBad} text={"Bad"} />
+            <h3>Anecdote of the day</h3>
+            <p>{anecdotes[selected]}</p>
+            <button onClick={handleShow}>Next anecdote</button>
+            <p>vote count : {votes[selected]}</p>
+            <button onClick={handleVote}>Vote</button>
+            <h3>Anecdote with most votes</h3>
 
-            <h3>Statistics</h3>
-            {good || neutral || bad ? (
-                <Statistics
-                    good={good}
-                    neutral={neutral}
-                    bad={bad}
-                    all={all}
-                    average={average}
-                    positive={positive}
-                />
-            ) : (
-                <p>No feedback given</p>
-            )}
+            <p>vote count MAX: {votes[indexOfMaxValue]}</p>
+            <p>{votes[indexOfMaxValue] ? anecdotes[indexOfMaxValue] : ""} </p>
         </>
     );
 };
